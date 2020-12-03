8607788edca1851dc7e8b26fd8acf4e7162b3cfd
xuchanghui <xuchanghui888@gmail.com>
2020-12-04 07:52:23 +0800

    float

--- zhufeng/29_webpack/dist/boudle.js
+++ zhufeng/29_webpack/dist/boudle.js
@@ -1,1 +1,17 @@
-console.log("hello"),document.getElementById("app").innerHTML="zfpx";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("console.log('hello')\ndocument.getElementById('app').innerHTML = 'zfpx'\n// npx 可以直接运行node_modules/.bin目录下面的命令\n// 通过配置package.json中的script \"build\":\"webpack\"\n\n//# sourceURL=webpack://29_webpack/./src/index.js?");
+/******/ })()
+;
